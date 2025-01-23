0e6f61f73eca1978fd37434e2f52a04e03886c9d
Igor Lapin <raynor73@gmail.com>
2025-01-23 13:35:14 +0000

    Sync

---
+++ RenderingEngine/Camera.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <glm/glm.hpp>
+
+namespace RenderingEngine {
+	class Camera {
+	public:
+		virtual float zNear() = 0;
+		virtual void zFar() = 0;
+		virtual const glm::vec4& clearColor() = 0;
+		virtual const glm::mat4& viewMatrix() = 0;
+		virtual const glm::mat4& projectionMatrix() = 0;
+		/*bool shouldClearDepth() const { return m_shouldClearDepth; }
+		void setShouldClearDepth(bool shouldClearDepth) { m_shouldClearDepth = shouldClearDepth; }
+		bool shouldClearColor() const { return m_shouldClearColor; }
+		void setShouldClearColor(bool shouldClearColor) { m_shouldClearColor = shouldClearColor; }*/
+	};
+}
--- RenderingEngine/Transformation.h
+++ RenderingEngine/Transformation.h
@@ -5,8 +5,8 @@
 namespace RenderingEngine {
 	class Transformation {
 	public:
-		virtual const glm::vec3& position() const = 0;
-		virtual const glm::quat& rotation() const = 0;
-		virtual const glm::vec3& scale() const = 0;
+		virtual const glm::vec3& position() = 0;
+		virtual const glm::quat& rotation() = 0;
+		virtual const glm::vec3& scale() = 0;
 	};
 }
